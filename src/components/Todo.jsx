3275c32a53fdb0a0aa4669cfef91d237400315ec
Javier Granados <jgranadosflores@gmail.com>
2019-03-22 00:11:44 +0100

    Add todo list

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,16 +2,30 @@ import React, { Fragment, useState } from 'react';
 
 const todo = () => {
   const [task, setTask] = useState('');
+  const [todoList, setTodoList] = useState(['Initial task']);
 
   const inputChangeHandler = event => {
     setTask(event.target.value);
   };
 
+  const todoAddHandler = () => {
+    if (task && task.trim() !== '') {
+      setTodoList(todoList.concat(task));
+    }
+    setTask('');
+  };
+
   return (
     <Fragment>
       <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={task} />
-      <button type="button">Add</button>
-      <ul />
+      <button type="button" onClick={todoAddHandler}>
+        Add task
+      </button>
+      <ul>
+        {todoList.map((element, i) => (
+          <li key={i}>{element}</li>
+        ))}
+      </ul>
     </Fragment>
   );
 };
