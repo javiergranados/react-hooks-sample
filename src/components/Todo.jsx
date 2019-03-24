e21cf011a4b75928251f6b249aca1a994a7a3748
Javier Granados <jgranadosflores@gmail.com>
2019-03-24 22:54:19 +0100

    Add library axios

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 const todo = () => {
   const [task, setTask] = useState('');
@@ -11,6 +12,10 @@ const todo = () => {
   const todoAddHandler = () => {
     if (task && task.trim() !== '') {
       setTodoList(todoList.concat(task));
+      axios
+        .post('https://react-hooks-sample.firebaseio.com/todoList.json', { description: task })
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
     }
     setTask('');
   };
