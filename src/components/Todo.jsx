e39b16860773a9c8ea00ffd02c34f0f73782ce35
Javier Granados <jgranadosflores@gmail.com>
2019-03-24 23:16:40 +0100

    Implement useEffect hook

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,19 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 
 const todo = () => {
   const [task, setTask] = useState('');
-  const [todoList, setTodoList] = useState(['Initial task']);
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://react-hooks-sample.firebaseio.com/todoList.json').then(result => {
+      const todos = Object.values(result.data).map((el, i) => ({
+        id: Object.keys(result.data)[i],
+        description: el.description,
+      }));
+      setTodoList(todos);
+    });
+  }, []);
 
   const inputChangeHandler = event => {
     setTask(event.target.value);
@@ -14,8 +24,8 @@ const todo = () => {
       setTodoList(todoList.concat(task));
       axios
         .post('https://react-hooks-sample.firebaseio.com/todoList.json', { description: task })
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
+        .then(result => console.log(result))
+        .catch(error => console.log(error));
     }
     setTask('');
   };
@@ -27,8 +37,8 @@ const todo = () => {
         Add task
       </button>
       <ul>
-        {todoList.map((element, i) => (
-          <li key={i}>{element}</li>
+        {todoList.map(element => (
+          <li key={element.id}>{element.description}</li>
         ))}
       </ul>
     </Fragment>
